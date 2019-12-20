f473d419003a4c2b4ad0df0f7d573e588801d956
David <davidfmf64@gmail.com>
2019-12-20 08:33:19 +0100

    modif 2

--- src/PruebaTimer.h
+++ src/PruebaTimer.h
@@ -1,9 +1,12 @@
 /*******************************************************************************
    Autor: DavidFuentes
-	
+
+   Esto es un aversion molona con el branch
 		
 *******************************************************************************/
 
 
 #define	s32		(signed int)
 #define u32		(unsigned int)
+#define u8		(unsigned char)
+#define s8		(signed char)
